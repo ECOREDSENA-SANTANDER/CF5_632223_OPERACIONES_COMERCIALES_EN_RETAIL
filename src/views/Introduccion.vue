cf245cfd7babadc6fb088ed6ff7739695940b0a0
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2024-10-10 11:01:10 -0500

    Ajuste Video Introducción Maquetación

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -13,7 +13,7 @@
 
     figure
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/c1LwAMyN3Xw&t=4s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/c1LwAMyN3Xw?si=tD6Z3Z13tGFHU56z" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen)
     
 </template>
 
